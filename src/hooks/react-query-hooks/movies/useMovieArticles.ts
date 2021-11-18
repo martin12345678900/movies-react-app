2a33e25874d868b33416c73767c56df456cc3c64
martin12345678900 <71081502+martin12345678900@users.noreply.github.com>
2021-11-18 22:09:59 +0200

    Update useMovieArticles.ts

--- src/hooks/react-query-hooks/movies/useMovieArticles.ts
+++ src/hooks/react-query-hooks/movies/useMovieArticles.ts
@@ -1,11 +1,13 @@
-import { useQuery, useQueryClient } from "react-query";
+import { useQuery } from "react-query";
 
 import { getMovieArticles } from "../../../services/requests";
 import { MovieArticle } from "../../../components/Details/types";
 
 
-const useGetMovieArticles = () => {
-    return useQuery<MovieArticle[], Error>(['movies'], getMovieArticles);
+const useGetMovieArticles = (selectFn: (data: MovieArticle[]) => MovieArticle[]) => {
+    return useQuery<MovieArticle[], Error>(['movies'], getMovieArticles, {
+        select: selectFn
+    });
 }
 
 export default useGetMovieArticles;
